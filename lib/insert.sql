ea5cac7ef1d02ffe4b79d78e814e7f5797df1e63
rolandobrown <rolando.brown@gmail.com>
2015-08-17 19:17:01 -0400

    insert values

---
+++ lib/insert.sql
@@ -0,0 +1,53 @@
+INSERT INTO series (title)
+VALUES
+("Of The Loved"),
+("Of The Hated");
+
+INSERT INTO books (title)
+VALUES
+("Harmonious"),
+("Harmonious 358"),
+("Remembrance"),
+("Growing Up Gideon"),
+("The Loved"),
+("The Hated");
+
+INSERT INTO characters (name)
+VALUES
+("Sagacious"),
+("Gideon"),
+("The Loved"),
+("The Hated"),
+("Samson"),
+("Laco"),
+("Precocious"),
+("Nubiaa");
+
+INSERT INTO subgenres (name)
+VALUES
+("Magical Realism"),
+("Science Fiction");
+
+INSERT INTO authors (name)
+VALUES
+("Rolando Brown"),
+("Grow");
+
+INSERT INTO character_books (character_id, book_id)
+VALUES
+(1, 2),
+(2, 3),
+(3, 4),
+(4, 5),
+(5, 6),
+(6, 7),
+(7, 8),
+(8, 9),
+(9, 10),
+(10, 11),
+(11, 12),
+(12, 13),
+(13, 14),
+(14, 15),
+(15, 16),
+(16, 1);
--- lib/querying.rb
+++ lib/querying.rb
@@ -1,17 +1,42 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+  "SELECT title, year FROM books WHERE series_id = 1"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+  "SELECT name, motto
+  FROM characters
+  ORDER BY LENGTH(motto) DESC
+  LIMIT 1"
 end
 
 def select_value_and_count_of_most_prolific_species
+  "SELECT species, MAX(species_count)
+  FROM (
+  SELECT species, COUNT(species) AS species_count
+  FROM characters
+  GROUP BY species)"
 end
 
 def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  JOIN series
+  ON authors.id = series.author_id
+  JOIN subgenres
+  ON subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
+  "SELECT title
+  FROM series
+  JOIN characters
+  ON series.id = characters.series_id
+  GROUP BY series_id
+  HAVING species = 'human'
+  ORDER BY COUNT(*) DESC
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
+  SELECT name,
 end
